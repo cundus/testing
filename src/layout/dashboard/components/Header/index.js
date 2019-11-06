4eeded69733321d2047647c30f48444d8e3292ee
aqil gifari <aqilgifari0@gmail.com>
2019-11-06 11:15:52 +0700

    layouting: split styles to .css

--- src/layout/dashboard/components/Header/index.js
+++ src/layout/dashboard/components/Header/index.js
@@ -3,6 +3,8 @@ import { Layout, Menu, Row, Col, Icon, Typography, Avatar } from 'antd';
 
 import Logo from '../../../../assets/xl.png';
 import Indonesia from '../../../../assets/indonesia-flag.png';
+import "antd/dist/antd.css";
+import "./header-styles.css";
 
 const { Text } = Typography;
 
@@ -11,42 +13,22 @@ const Header = (props) => {
 
   return (
     <Layout.Header
-      style={{
-      background: '#fff',
-      padding: 0,
-      position: 'fixed',
-      left: 0, width: '100%',
-      boxShadow: '0px 0px 40px 0px rgba(82, 63, 105, 0.1)',
-      height: '80px',
-      zIndex: '999'
-      }}
+      className="headerContainer"
     >
       <Row
         justify="space-between"
         type="flex"
-        style={{
-          display: 'flex',
-          alignItems: 'flex-end',
-          justifyContent: 'space-evenly'
-        }}
+        className="headerWrapper"
       >
         <Col sm={12} xs={24}>
           <Menu
             theme="light"
             mode="horizontal"
             defaultSelectedKeys={['1']}
-            style={{
-              lineHeight: '64px',
-              borderBottom: 'none'
-            }}
+            className="menuWrapper"
           >
             <Icon
-              style={{
-                fontSize: 18,
-                padding: '0 24px',
-                cursor: 'pointer',
-                transition: 'color 0.3s'
-              }}
+              className="drawerIcon"
               type={collapsed ? 'align-right' : 'alignt-left'}
               onClick={toggle}
             />
@@ -60,15 +42,15 @@ const Header = (props) => {
               }
             >
               <Menu.Item key="default">
-              <Icon type="plus-circle" theme="filled" style={{ fontSize: '18px' }} />
+              <Icon type="plus-circle" theme="filled" className="dropdownItem" />
                 Create KPI
               </Menu.Item>
               <Menu.Item key="fluid">
-                <Icon type="plus-circle" style={{ fontSize: '18px' }} />
+                <Icon type="plus-circle" className="dropdownItem" />
                 Create Non KPI
               </Menu.Item>
               <Menu.Item key="asaid">
-                <Icon type="search" style={{ fontSize: '18px' }} />
+                <Icon type="search" className="dropdownItem" />
                 View My KPI
               </Menu.Item>
             </Menu.SubMenu>
@@ -84,17 +66,14 @@ const Header = (props) => {
           <Menu
             theme="light"
             mode="horizontal"
-            style={{
-              lineHeight: '64px',
-              borderBottom: 'none'
-            }}
+            className="menuWrapper"
           >
           <Row type="flex" justify="space-around" align="middle">
             <Menu.Item key="6">
               <Icon style={{ fontSize: 18 }} type="bell" />
             </Menu.Item>
             <Menu.Item key="7">
-              <img src={Indonesia} alt="flag" style={{ borderRadius: '4px' }} />
+              <img src={Indonesia} alt="flag" className="flagIcon" />
             </Menu.Item>
             <Menu.Item key="8">
               <Text>Hi, John Doe</Text>
