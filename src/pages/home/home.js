2b25b3e82cfb3fa2754d207237c2a577298c3794
geraldsamosir <gerald.unforgiven@gmail.com>
2019-11-14 12:19:01 +0700

    done homepages

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,10 +2,15 @@ import React, { Component } from "react";
 import  Layout from '../../layout/dashboard/index';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserFriends } from '@fortawesome/free-solid-svg-icons';
-import { Row, Col, Button } from 'antd';
+import { Row, Col, Button, Icon } from 'antd';
 import  './home-styles.scss';
 
-
+// icons list
+import collaborationIcon from '../../assets/icons/collaboration.svg'; 
+import CustomerIcon from  '../../assets/icons/customer.svg';
+import FileIcon from  '../../assets/icons/file.svg';
+import UsersIcon from '../../assets/icons/users.svg';
+import { file } from "@babel/types";
 
 class Home extends Component {
   render(){
@@ -14,25 +19,29 @@ class Home extends Component {
             <Layout>
               <Row style={{paddingTop:50}}>
                 <Col xl={6} lg={6} md={6} xs={24} className='grid'>
-                <FontAwesomeIcon icon={faUserFriends} className='purple' style={{height:120, width:120}}/>
+                  <br/><br/>
+                  <img src={FileIcon} className='purple' style={{width:120, height: 120}}/>
                   <h1>Planning</h1>
                   <p className='qoute-text'>Create New KPI or Cascade</p>
                   <Button shape='round' className='homeBtn  purpleBtn'>Create KPI</Button>
                 </Col>
                 <Col xl={6} lg={6} md={6} xs={24} className='grid'>
-                  <FontAwesomeIcon icon={faUserFriends} className='pink' style={{ height:120, width:120}}/>
+                  <br/><br/>
+                  <img src={UsersIcon} className='pink' style={{width:120, height: 120}}/>
                   <h1>Monitoring</h1>
                   <p className='qoute-text'>Feedbask session with Superior</p>
                   <Button shape='round' className='homeBtn  pinkBtn'>View Feedback Session</Button>
                 </Col>
                 <Col xl={6} lg={6} md={6} xs={24} className='grid'>
-                 <FontAwesomeIcon icon={faUserFriends} className='yellow' style={{ height:120, width:120}}/>
+                  <br/><br/>
+                  <img src={CustomerIcon} className='yellow' style={{width:120, height: 120}}/>
                   <h1>Appraisal</h1>
                   <p className='qoute-text'>View your final performance rating</p>
                   <Button  shape='round' className='homeBtn  yellowBtn'>View My Final Performance</Button>
                 </Col>
                 <Col xl={6} lg={6} md={6} xs={24} className='grid'>
-                <FontAwesomeIcon icon={faUserFriends} className='green' style={{ height:120, width:120}}/>
+                  <br/><br/>
+                  <img src={collaborationIcon}  className='green' style={{width:120, height: 120}}/>
                   <h1>My Team</h1>
                   <p className='qoute-text'>View your team's performance</p>
                   <Button className='homeBtn  greenBtn' shape='round'>View My Team Performance</Button>
