3e125bfdcd9c8455a1f6d02ad53d31400951ccc9
aqil gifari <aqilgifari0@gmail.com>
2019-11-14 12:17:10 +0700

    make table components dynamically

--- src/components/dataTable/index.js
+++ src/components/dataTable/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Table, Input, Button, Popconfirm, Form } from "antd";
+import { Table, Input, InputNumber, Button, Form } from "antd";
 import "antd/dist/antd.css";
 import "./dataTable-style.scss";
+import { useMediaQuery } from "react-responsive";
 
 const { TextArea } = Input;
 
@@ -29,18 +30,34 @@ class EditableCell extends React.Component {
 
   renderCell = form => {
     this.form = form;
-    const { /*children,*/ dataIndex, record/*, title*/ } = this.props;
+    const {
+      /*children,*/ dataIndex,
+      record,
+      placeholder,
+      type /*, title*/
+    } = this.props;
     // const { editing } = this.state;
     return (
       <Form.Item style={{ margin: 0 }}>
         {form.getFieldDecorator(dataIndex, {
           initialValue: record[dataIndex]
         })(
-          <TextArea
-            ref={node => (this.input = node)}
-            autoSize={{ minRows: 3, maxRows: 5 }}
-            onChange={this.change}
-          />
+          type === "number" ? (
+            <InputNumber
+              ref={node => (this.input = node)}
+              placeholder={placeholder}
+              type={type}
+              autoSize={{ minRows: 3, maxRows: 5 }}
+              onChange={this.change}
+            />
+          ) : (
+            <TextArea
+              ref={node => (this.input = node)}
+              placeholder={placeholder}
+              autoSize={{ minRows: 3, maxRows: 5 }}
+              onChange={this.change}
+            />
+          )
         )}
       </Form.Item>
     );
@@ -69,107 +86,10 @@ class EditableCell extends React.Component {
   }
 }
 
-class DataTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.columns = [
-      {
-        title: "KPI",
-        dataIndex: "kpi",
-        editable: true
-      },
-      {
-        title: "2019 Baseline",
-        dataIndex: "baseline",
-        editable: true
-      },
-      {
-        title: "Weight (100%)",
-        dataIndex: "weight",
-        editable: true
-      },
-      {
-        title: "L1",
-        dataIndex: "l1",
-        editable: true
-      },
-      {
-        title: "L2",
-        dataIndex: "l2",
-        editable: true
-      },
-      {
-        title: "L3",
-        dataIndex: "l3",
-        editable: true
-      },
-      {
-        title: "operation",
-        dataIndex: "operation",
-        render: (text, record) =>
-          this.state.dataSource.length >= 1 ? (
-            <Popconfirm
-              title="Sure to delete?"
-              onConfirm={() => this.handleDelete(record.key)}
-            >
-              <Button>Delete</Button>
-            </Popconfirm>
-          ) : null
-      }
-    ];
+const DataTable = (props) => {
+    const { dataSource, handleChange, handleAdd, columns } = props;
 
-    this.state = {
-      dataSource: [
-        {
-          key: 0,
-          kpi: "",
-          baseline: "",
-          weight: "",
-          l1: "",
-          l2: "",
-          l3: ""
-        }
-      ],
-      count: 1
-    };
-  }
-
-  handleDelete = key => {
-    const dataSource = [...this.state.dataSource];
-    this.setState({ dataSource: dataSource.filter(item => item.key !== key) });
-  };
-
-  handleAdd = () => {
-    const { count, dataSource } = this.state;
-    const newData = {
-      key: count,
-      kpi: "",
-      baseline: "",
-      weight: "",
-      l1: "",
-      l2: "",
-      l3: ""
-    };
-    this.setState({
-      dataSource: [...dataSource, newData],
-      count: count + 1
-    });
-  };
-
-  handleChange = row => {
-    const newData = [...this.state.dataSource];
-    const index = newData.findIndex(item => row.key === item.key);
-    const item = newData[index];
-    newData.splice(index, 1, {
-      ...item,
-      ...row
-    });
-    this.setState({ dataSource: newData });
-  };
-
-  render() {
-    const { dataSource } = this.state;
-    // console.log(dataSource);
+    const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1224 });
 
     const components = {
       body: {
@@ -177,7 +97,7 @@ class DataTable extends React.Component {
         cell: EditableCell
       }
     };
-    const columns = this.columns.map(col => {
+    const columnList = columns.map(col => {
       if (!col.editable) {
         return col;
       }
@@ -188,30 +108,28 @@ class DataTable extends React.Component {
           editable: col.editable,
           dataIndex: col.dataIndex,
           title: col.title,
-          handleChange: this.handleChange
+          type: col.type,
+          placeholder: col.placeholder,
+          numberType: col.numberType,
+          handleChange: handleChange
         })
       };
     });
     return (
       <div>
-          <Table
-            components={components}
-            rowClassName={() => "editable-row"}
-            bordered
-            dataSource={dataSource}
-            columns={columns}
-            scroll={{ x: true}}
-          />
-          <Button
-            onClick={this.handleAdd}
-            type="primary"
-            style={{ marginBottom: 16 }}
-          >
-            Add a row
-          </Button>
+        <Table
+          components={components}
+          rowClassName={() => "editable-row"}
+          bordered
+          dataSource={dataSource}
+          columns={columnList}
+          scroll={isDesktopOrLaptop ? { x: false } : { x: true }}
+        />
+        <Button onClick={handleAdd} type="primary" style={{ marginBottom: 16 }}>
+          Add a row
+        </Button>
       </div>
     );
   }
-}
 
 export default DataTable;
