15875578c30f88c52a72c1f1af6e46c3f6235a0d
aqil gifari <aqilgifari0@gmail.com>
2019-11-14 13:21:33 +0700

    fetch: sidebar from routes

--- src/layout/dashboard/components/Sidebar/index.js
+++ src/layout/dashboard/components/Sidebar/index.js
@@ -1,72 +1,80 @@
-import React from 'react';
-import { Layout, Menu, Icon, Typography } from 'antd';
+import React from "react";
+import { Layout, Menu, Icon, /*Typography*/ } from "antd";
+import { Link } from "react-router-dom";
+import Router from "../../../../Router";
 
 const { Sider } = Layout;
-const { Text } = Typography;
 
-const Sidebar = (props) => {
+const Sidebar = props => {
+  const mainRouter = Router.filter(x => x.menuLevel === 1);
+  const pathlocation = window.location.pathname;
   const { collapsed, toggle } = props;
   return (
-    <Sider style={{ zIndex: 1, boxShadow: '0px 1px 9px -3px rgba(0, 0, 0, 0.75)' }} trigger={null} collapsible collapsedWidth={0} collapsed={collapsed}>
+    <Sider
+      style={{ zIndex: 1, boxShadow: "0px 1px 9px -3px rgba(0, 0, 0, 0.75)" }}
+      trigger={null}
+      collapsible
+      collapsedWidth={0}
+      collapsed={collapsed}
+    >
       <Menu
-        style={{ width: 275, height: '100vh' }}
-        defaultSelectedKeys={['1']}
-        // defaultOpenKeys={['sub1']}
+        style={{ width: 275, height: "100vh" }}
+        selectedKeys={[pathlocation]}
         mode="inline"
         theme="light"
       >
-        <div style={{ padding: 4, textAlign: 'right' }}>
-          <Icon type="close" style={{ color: 'white', background: 'lightgray', padding: 4, borderRadius: 4 }} onClick={toggle} />
+        <div style={{ padding: 4, textAlign: "right" }}>
+          <Icon
+            type="close"
+            style={{
+              color: "white",
+              background: "lightgray",
+              padding: 4,
+              borderRadius: 4
+            }}
+            onClick={toggle}
+          />
         </div>
-          <Menu.Item key="1">
-            <Icon type="setting" />
-            Layout Builder
-          </Menu.Item>
-          <div style={{ padding: 24 }}>
-            <Text><b>Components</b></Text>
-          </div>
-          <Menu.Item key="2">
-            <Icon type="database" />
-            Datatables
-        </Menu.Item>
-          <Menu.Item key="3">
-            <Icon type="bulb" />
-            Buttons
-        </Menu.Item>
-          <Menu.Item key="4">
-            <Icon type="container" />
-            Portlets
-        </Menu.Item>
-          <Menu.Item key="5">
-            <Icon type="bg-colors" />
-            Timeline
-        </Menu.Item>
-          <div style={{ padding: 24 }}>
-            <Text><b>Page Elements</b></Text>
-          </div>
-          <Menu.Item key="6">
-            <Icon type="code" />
-            Widgetts
-        </Menu.Item>
-          <Menu.Item key="7">
-            <Icon type="calendar" />
-            Calenders
-        </Menu.Item>
-          <Menu.Item key="8">
-            <Icon type="cloud" />
-            Prising Tables
-        </Menu.Item>
-          <Menu.Item key="9">
-            <Icon type="calendar" />
-            Invoice
-        </Menu.Item>
-          <Menu.Item key="10">
-            <Icon type="crown" />
-            Custom Pages
-        </Menu.Item>
-      </Menu>
+        {mainRouter.map((r, i) => {
+              // check is has child
+              const childsRoutes = Router.filter(rc => rc.parent === r.name);
+              if (childsRoutes.length === 0) {
+                return (
+                  <Menu.Item key={`${r.path}`}>
+                    <Link to={r.path}>{r.viewName}</Link>
+                  </Menu.Item>
+                );
+              } else {
+                return (
+                  <Menu.SubMenu
+                    key={`${r.name}-${i}`}
+                    title={
+                      <span className="submenu-title-wrapper">
+                        {r.viewName}
+                      </span>
+                    }
+                  >
+                    {childsRoutes.map((rc, i) => {
+                      return (
+                        <Menu.Item key={`${rc.path}`}>
+                          <Link to={rc.path}>
+                            <Icon
+                              type={`${rc.icon}`}
+                              theme={`${rc.theme}`}
+                              className="dropdownItem"
+                            />
+                            {rc.viewName}
+                          </Link>
+                        </Menu.Item>
+                      );
+                    })}
+                  </Menu.SubMenu>
+                );
+              }
+            })}
+          </Menu>
     </Sider>
-  )
-}
+  );
+};
 
 export default Sidebar;
