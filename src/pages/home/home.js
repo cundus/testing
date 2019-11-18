0d7f9a43b6b71964394ef3e07218c90bfe7aa4a5
aqil gifari <aqilgifari0@gmail.com>
2019-11-18 13:30:04 +0700

    get all data create-kpi planning

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import  Layout from '../../layout/dashboard/index';
 import { Row, Col, Button/*, Icon */} from 'antd';
 import  './home-styles.scss';
 
@@ -13,8 +12,7 @@ import UsersIcon from '../../assets/icons/users.svg';
 class Home extends Component {
   render(){
       return(
-        <div>
-            {/* <Layout> */}
+        <div style={{textAlign: "center"}}>
               <Row style={{paddingTop:50}}>
                 <Col xl={6} lg={6} md={6} xs={24} className='grid'>
                   <br/><br/>
@@ -45,7 +43,6 @@ class Home extends Component {
                   <Button className='homeBtn  greenBtn' shape='round'>View My Team Performance</Button>
                 </Col>
               </Row>
-            {/* </Layout> */}
         </div>
       )
   }
--- src/pages/planning/create-kpi/components/cascade-previous.js
+++ src/pages/planning/create-kpi/components/cascade-previous.js
@@ -1,127 +1,55 @@
 import React, { Component } from "react";
 import { DataTable } from "../../../../components";
-import { Button, Popconfirm } from "antd";
+import { Button, Checkbox } from "antd";
 
 class CascadePrevious extends Component {
   constructor(props) {
     super(props);
     this.columns = [
+      ...this.props.columnList,
       {
-        title: "KPI",
-        dataIndex: "kpi",
-        placeholder: "Enter KPI subject",
-        editable: false,
-      },
-      {
-        title: "2020 Baseline",
-        dataIndex: "baseline",
-        placeholder: "Enter 2020 baseline",
-        editable: false,
-      },
-      {
-        title: "Weight (%)",
-        dataIndex: "weight",
-        placeholder: "Enter KPI Weight",
-        type: "number",
-        editable: false,
-      },
-      {
-        title: "L1",
-        dataIndex: "l1",
-        placeholder: "Enter Level 1",
-        editable: false,
-      },
-      {
-        title: "L2",
-        dataIndex: "l2",
-        placeholder: "Enter Level 2",
-        editable: false,
-      },
-      {
-        title: "L3",
-        dataIndex: "l3",
-        placeholder: "Enter Level 3",
-        editable: false,
-      },
-      {
-        title: "operation",
-        dataIndex: "operation",
+        title: "",
+        dataIndex: "action",
         action: true,
         render: (text, record) =>
-          this.state.dataSource.length >= 1 ? (
-            <Popconfirm
-              title="Sure to delete?"
-              onConfirm={() => this.handleDelete(record.key)}
-            >
-              <Button>Delete</Button>
-            </Popconfirm>
+          this.props.dataCascadePartner.length >= 1 ? (
+            <Checkbox
+              onChange={e => this.props.handleSelectData(record)}
+              checked={this.checkStatus(record)}
+            />
           ) : null
       }
     ];
-
-    this.state = {
-      dataSource: [
-        {
-          key: 0,
-          kpi: "",
-          baseline: "",
-          weight: "",
-          l1: "",
-          l2: "",
-          l3: ""
-        }
-      ],
-      count: 1
-    };
   }
 
-  handleDelete = key => {
-    const dataSource = [...this.state.dataSource];
-    this.setState({ dataSource: dataSource.filter(item => item.key !== key) });
-  };
-
-  handleAdd = () => {
-    const { count, dataSource } = this.state;
-    const newData = {
-      key: count,
-      kpi: "",
-      baseline: "",
-      weight: "",
-      l1: "",
-      l2: "",
-      l3: ""
-    };
-    this.setState({
-      dataSource: [...dataSource, newData],
-      count: count + 1
-    });
-  };
-
-  handleChange = row => {
-    const newData = [...this.state.dataSource];
-    const index = newData.findIndex(item => row.key === item.key);
-    const item = newData[index];
-    newData.splice(index, 1, {
-      ...item,
-      ...row
-    });
-    this.setState({ dataSource: newData });
+  checkStatus = record => {
+    const { dataSelectedCascade } = this.props;
+    const dataChecking = dataSelectedCascade.filter(item => item.key === record.key);
+    if (dataChecking.length !== 0) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   render() {
-    const { dataSource } = this.state;
-    const { columns, handleAdd, handleChange, handleDelete } = this;
+    const { dataCascadePrevious, handleSaveDraft } = this.props;
+    const { columns } = this;
+
     return (
       <div>
-        {/* <Layout> */}
         <DataTable
           columns={columns}
-          dataSource={dataSource}
-          handleAdd={handleAdd}
-          handleChange={handleChange}
-          handleDelete={handleDelete}
+          dataSource={dataCascadePrevious}
         />
-        {/* </Layout> */}
+        <div style={{ textAlign: "center" }}>
+          <Button
+            onClick={handleSaveDraft}
+            style={{ margin: 10 }}
+          >
+            Save Draft
+          </Button>
+        </div>
       </div>
     );
   }
