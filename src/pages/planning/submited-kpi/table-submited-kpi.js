49f712a319f14931fc71e25bba7db75ec38e3259
aqil gifari <aqilgifari0@gmail.com>
2019-11-20 17:48:24 +0700

    submitted kpi pages fix

--- src/pages/planning/submited-kpi/table-submited-kpi.js
+++ src/pages/planning/submited-kpi/table-submited-kpi.js
@@ -15,38 +15,38 @@ class TableDrafKPI extends Component {
         title: 'KPI Subject',
         dataIndex: 'kpi',
         placeholder: 'Enter 2020 baseline',
-        editable: true
+        editable: false
       },
       {
         title: '2019 Baseline',
         dataIndex: 'baseline',
         placeholder: 'Enter 2019 baseline',
-        editable: true
+        editable: false
       },
       {
         title: 'Weight (100%)',
         dataIndex: 'weight',
         placeholder: 'Enter KPI Weight',
         type: 'number',
-        editable: true
+        editable: false
       },
       {
         title: 'L1',
         dataIndex: 'l1',
         placeholder: 'Enter Level 1',
-        editable: true
+        editable: false
       },
       {
         title: 'L2',
         dataIndex: 'l2',
         placeholder: 'Enter Level 2',
-        editable: true
+        editable: false
       },
       {
         title: 'L3',
         dataIndex: 'l3',
         placeholder: 'Enter Level 3',
-        editable: true
+        editable: false
       }
     ];
   }
