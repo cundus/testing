3c268a2c745d5391660e9a44dc7e3342ddce7dfc
aqil gifari <aqilgifari0@gmail.com>
2019-11-22 09:44:18 +0700

    adding actiontype for get latest goal kpi

--- src/redux/action.type.js
+++ src/redux/action.type.js
@@ -1,7 +1,11 @@
-// DRAFT
+// KPI
 export const SAVE_DRAFT = 'SAVE_DRAFT';
 export const SAVE_DRAFT_SUCCESS = 'SAVE_DRAFT_SUCCESS';
 export const SAVE_DRAFT_FAILED = 'SAVE_DRAFT_FAILED';
+
+export const GET_LATEST_KPI = 'GET_LATEST_KPI_DRAFT';
+export const GET_LATEST_KPI_SUCCESS = 'GET_LATEST_KPI_SUCCESS';
+export const GET_LATEST_KPI_FAILED = 'GET_LATEST_KPI_FAILED';
 // AUTH
 export const getUserInfo = 'GET_USER_INFO';
 export const errGetUserInfo = 'ERR_GET_USER_INFO';
