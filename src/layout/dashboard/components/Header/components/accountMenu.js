5ada56dceded78777ed1189a0421f85e4e272254
aqil gifari <aqilgifari0@gmail.com>
2019-12-02 10:35:01 +0700

    adding: dropdown message

---
+++ src/layout/dashboard/components/Header/components/accountMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Menu } from 'antd';
+
+const accountMenu = (
+  <Menu>
+    <Menu.Item>
+        MEssage
+    </Menu.Item>
+  </Menu>
+  );
+
+export default accountMenu;
