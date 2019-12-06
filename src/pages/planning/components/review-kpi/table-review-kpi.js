952c0442894b2cc8c93bc322fa212c9531b5f9a3
aqil gifari <aqilgifari0@gmail.com>
2019-12-06 15:49:22 +0700

    add review kpi

---
+++ src/pages/planning/components/review-kpi/table-review-kpi.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { DataTable } from '../../../../components';
+
+class TableDrafKPI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columns: []
+    };
+    this.getColumns();
+  }
+
+  getColumns = async () => {
+    const { kpiReducers } = this.props;
+    const { dataMetrics } = kpiReducers;
+    const newColumns = [
+      {
+        title: 'Cascading / Self KPI',
+        dataIndex: 'typeKpi',
+        align: 'center',
+        width: 200,
+        placeholder: 'Cascading/Self KPI',
+        editable: false
+      },
+      {
+        title: 'KPI Subject',
+        dataIndex: 'description',
+        placeholder: 'Enter KPI Subject',
+        align: 'center',
+        width: 200,
+        editable: false
+      },
+      {
+        title: 'Baseline',
+        dataIndex: 'baseline',
+        placeholder: 'Enter baseline',
+        align: 'center',
+        width: 200,
+        editable: false
+      },
+      {
+        title: 'Weight (%)',
+        dataIndex: 'weight',
+        placeholder: 'Enter KPI Weight',
+        align: 'center',
+        type: 'number',
+        editable: false
+      }
+    ];
+    // eslint-disable-next-line array-callback-return
+    await dataMetrics.map((itemMetric) => {
+      const data = {
+        title: itemMetric.label,
+        dataIndex: itemMetric.label,
+        placeholder: `Enter Level ${itemMetric.orderNo}`,
+        align: 'center',
+        width: 150,
+        editable: false
+      };
+      newColumns.push(data);
+    });
+    const Feedback = {
+      title: 'Feedback',
+      dataIndex: 'feedback',
+      placeholder: 'Enter KPI Feedback',
+      align: 'center',
+      color: 'rgb(250, 247, 187)',
+      editable: false
+    };
+    await newColumns.push(Feedback);
+    this.setState({
+      columns: newColumns
+    });
+  }
+
+  render() {
+    const { columns } = this.state;
+    const {
+      dataSource,
+      loading
+    } = this.props;
+    return (
+      <div>
+        <DataTable
+          columns={columns}
+          loading={loading}
+          datasource={dataSource}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  kpiReducers: state.kpiReducers
+});
+
+const mapDispatchToProps = (dispatch) => ({});
+
+const connectToComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TableDrafKPI);
+
+export default connectToComponent;
+
+TableDrafKPI.propTypes = {
+  dataSource: PropTypes.instanceOf(Array),
+  kpiReducers: PropTypes.instanceOf(Object),
+  loading: PropTypes.bool
+};
--- src/routes/RouteGenerator/index.js
+++ src/routes/RouteGenerator/index.js
@@ -19,11 +19,11 @@ const RenderedRoute = (Component, child ) => (props) => {
   // const { isLogin } = state.auth;
   const { location } = props;
   const { pathname } = location;
-  const token = localStorage.getItem('token');
-  if ((pathname === '' ||
+  // const token = localStorage.getItem('token');
+  if (pathname === '' ||
   pathname === '/' ||
   pathname === '/planning' ||
-  pathname === '/planning/') && token !== null) {
+  pathname === '/planning/') {
     return (<Redirect to="/home" />);
   }
   // if (token === null && !isLogin && pathname !== '/login') {
