c06e9f9a2bc5a167875548c41a7d7a7aebf4f335
aqil gifari <aqilgifari0@gmail.com>
2019-12-27 18:24:24 +0700

    add validator / data type (qty/qtv)

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -4,86 +4,75 @@ export const metricValidator = (data) => [
     message: `${data.title} is required`
   },
   {
-    pattern: new RegExp('^[1]*?(?<Percentage>[1-9][0-9]?|100)?$'),
-    message: 'Value must between 1 to 100'
-  },
-  {
     validator: async (rule, value, callback, source) => {
       const L1 = data.form.getFieldValue(`${data.type}[${data.indexarr}].L1`);
       const L2 = data.form.getFieldValue(`${data.type}[${data.indexarr}].L2`);
       const L3 = data.form.getFieldValue(`${data.type}[${data.indexarr}].L3`);
-      if (L1 && L2 && L3) {
-        if (parseFloat(L1) < parseFloat(L3)) {
-          switch (data.title) {
-            case 'L1':
+      const regexNumber = new RegExp('^[0-9]*$');
+      const regexZero = new RegExp('^[0-0]*$');
+      switch (data.title) {
+        case 'L1':
+          if (regexZero.test(L1)) {
+            callback('Value must be not a zero');
+          } else if (!regexNumber.test(L1)) {
+            callback('Value must be a number');
+          } else if (L1 && L2 && L3) {
+            if (parseFloat(L1) < parseFloat(L2) < parseFloat(L3)) {
               if (parseFloat(L1) >= parseFloat(L2)) {
                 callback('Value must last than L2');
               }
-              break;
-            case 'L2':
+            } else if (parseFloat(L1) > parseFloat(L2) > parseFloat(L3)) {
+              if (parseFloat(L1) <= parseFloat(L2)) {
+                callback('Value must last than L2');
+              }
+            } else {
+              callback('L1-L3 Must be Ascending/Descending');
+            }
+          }
+          break;
+        case 'L2':
+          if (regexZero.test(L2)) {
+            callback('Value must be not a zero');
+          } else if (!regexNumber.test(L2)) {
+            callback('Value must be a number');
+          } else if (L1 && L2 && L3) {
+            if (parseFloat(L1) < parseFloat(L2) < parseFloat(L3)) {
               if (parseFloat(L2) >= parseFloat(L3)) {
                 callback('Value must last than L3');
               } else if (parseFloat(L2) <= parseFloat(L1)) {
                 callback('Value must bigger than L1');
               }
-              break;
-            case 'L3':
-              if (parseFloat(L3) <= parseFloat(L2)) {
-                callback('Value must bigger than L2');
-              }
-              break;
-            default:
-                  // code block
-          }
-        } else if (parseFloat(L1) > parseFloat(L3)) {
-          switch (data.title) {
-            case 'L1':
-              if (parseFloat(L1) <= parseFloat(L2)) {
-                callback('Value must bigger than L2');
-              }
-              break;
-            case 'L2':
+            } else if (parseFloat(L1) > parseFloat(L2) > parseFloat(L3)) {
               if (parseFloat(L2) <= parseFloat(L3)) {
                 callback('Value must bigger than L3');
               } else if (parseFloat(L2) >= parseFloat(L1)) {
                 callback('Value must last than L1');
               }
-              break;
-            case 'L3':
+            } else {
+              callback('L1-L3 Must be Ascending/Descending');
+            }
+          }
+          break;
+        case 'L3':
+          if (regexZero.test(L1)) {
+            callback('Value must be not a zero');
+          } else if (!regexNumber.test(L1)) {
+            callback('Value must be a number');
+          } else if (L1 && L2 && L3) {
+            if (parseFloat(L1) < parseFloat(L2) < parseFloat(L3)) {
+              if (parseFloat(L3) <= parseFloat(L2)) {
+                callback('Value must bigger than L2');
+              }
+            } else if (parseFloat(L1) > parseFloat(L2) > parseFloat(L3)) {
               if (parseFloat(L3) >= parseFloat(L2)) {
                 callback('Value must last than L2');
               }
-              break;
-            default:
-                  // code block
+            } else {
+              callback('L1-L3 Must be Ascending/Descending');
+            }
           }
-        } else if (parseFloat(L1) === parseFloat(L3)) {
-          switch (data.title) {
-            case 'L1':
-              if (parseFloat(L1) === parseFloat(L2)) {
-                callback('Value can\'t be same with L2');
-              } else if (parseFloat(L1) === parseFloat(L3)) {
-                callback('Value can\'t be same with L3');
-              }
-              break;
-            case 'L2':
-              if (parseFloat(L2) === parseFloat(L1)) {
-                callback('Value can\'t be same with L1');
-              } else if (parseFloat(L2) === parseFloat(L3)) {
-                callback('Value can\'t be same with L3');
-              }
-              break;
-            case 'L3':
-              if (parseFloat(L3) === parseFloat(L2)) {
-                callback('Value can\'t be same with L2');
-              } else if (parseFloat(L3) === parseFloat(L1)) {
-                callback('Value can\'t be same with L1');
-              }
-              break;
-            default:
-                  // code block
-          }
-        }
+          break;
+        default:
       }
     }
   }
