d3e00a647b4a55409cf398ec2b6838a2c4b104b1
aqil gifari <aqilgifari0@gmail.com>
2020-01-02 13:51:16 +0700

    fix validation wording

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -116,10 +116,8 @@ export const metricValidatorText = (data) => [
       switch (data.title) {
         case 'Below':
           if (Below) {
-            if (regexZero.test(Below)) {
-              callback('Value must be not a zero');
-            } else if (regexNumber.test(Below)) {
-              callback('Value must not a number');
+            if (regexNumber.test(Below)) {
+              callback('Value must contain number and letter, or letter only.');
             } else if (Below && Meet && Exceed) {
               if (Below === Meet) {
                 callback('Value must different with Meet');
@@ -131,10 +129,8 @@ export const metricValidatorText = (data) => [
           break;
         case 'Meet':
           if (Meet) {
-            if (regexZero.test(Meet)) {
-              callback('Value must be not a zero');
-            } else if (regexNumber.test(Meet)) {
-              callback('Value must not a number');
+            if (regexNumber.test(Meet)) {
+              callback('Value must contain number and letter, or letter only.');
             } else if (Below && Meet && Exceed) {
               if (Meet === Below) {
                 callback('Value must different with Below');
@@ -146,10 +142,8 @@ export const metricValidatorText = (data) => [
           break;
         case 'Exceed':
           if (Exceed) {
-            if (regexZero.test(Exceed)) {
-              callback('Value must be not a zero');
-            } else if (regexNumber.test(Exceed)) {
-              callback('Value must not a number');
+            if (regexNumber.test(Exceed)) {
+              callback('Value must contain number and letter, or letter only.');
             } else if (Below && Meet && Exceed) {
               if (Exceed === Meet) {
                 callback('Value must different with Meet');
