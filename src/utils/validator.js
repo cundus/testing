5c94c022a5844885919816bbb8657dacc717a75f
aqil gifari <aqilgifari0@gmail.com>
2020-01-07 16:18:36 +0700

    fix dynamic validator

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -5,78 +5,56 @@ export const metricValidator = (data) => [
   },
   {
     validator: async (rule, value, callback, source) => {
-      const Below = data.form.getFieldValue(`${data.type}[${data.indexarr}].Below`);
-      const Meet = data.form.getFieldValue(`${data.type}[${data.indexarr}].Meet`);
-      const Exceed = data.form.getFieldValue(`${data.type}[${data.indexarr}].Exceed`);
+      const { record } = data;
+      let dataMetrics = record.metrics.map((metric) => {
+        return `{"${metric.label}":
+        "${data.form.getFieldValue(
+          `${data.type}[${data.indexarr}].${metric.label}`
+          )}"}`;
+      });
+      dataMetrics = JSON.parse(`[${dataMetrics.toString()}]`);
+      dataMetrics = dataMetrics.reduce((result, current) => {
+        return Object.assign(result, current);
+      }, {});
+      let isFilled = true;
+      let isSortedAsc = false;
+      let isSortedDesc = false;
+      const datas = Object.keys(dataMetrics);
       const regexNumber = new RegExp('^[0-9]*$');
       const regexZero = new RegExp('^[0-0]*$');
-      if (Below && Meet && Exceed) {
-        switch (data.title) {
-          case 'Below':
-            if (Below) {
-              if (regexZero.test(Below)) {
-                callback('Value must be not a zero');
-              } else if (!regexNumber.test(Below)) {
-                callback('Value must be a number');
-              } else if (Below && Meet && Exceed) {
-                if (parseFloat(Below) < parseFloat(Meet)) {
-                  if (!(parseFloat(Meet) < parseFloat(Exceed))) {
-                    callback('Target Must be Ascending/Descending');
-                  }
-                } else if (parseFloat(Below) > parseFloat(Meet)) {
-                  if (!(parseFloat(Meet) > parseFloat(Exceed))) {
-                    callback('Target Must be Ascending/Descending');
-                  }
-                } else {
-                  callback('Target Must be Ascending/Descending');
-                }
+      if (value) {
+        if (regexZero.test(value)) {
+          callback('Value must be not a zero');
+        } else if (!regexNumber.test(value)) {
+          callback('Value must be a number');
+        } else {
+          for (let index = 0; index < datas.length; index++) {
+            if (!dataMetrics[datas[index]]) {
+              isFilled = false;
+              break;
+            }
+          }
+          if (isFilled) {
+            for (let index = 0; index < datas.length - 1; index++) {
+              if (parseFloat(dataMetrics[datas[index]]) < parseFloat(dataMetrics[datas[index + 1]])) {
+                isSortedAsc = true;
+              } else {
+                isSortedAsc = false;
+                break;
               }
             }
-            break;
-          case 'Meet':
-            if (Meet) {
-              if (regexZero.test(Meet)) {
-                callback('Value must be not a zero');
-              } else if (!regexNumber.test(Meet)) {
-                callback('Value must be a number');
-              } else if (Below && Meet && Exceed) {
-                if (parseFloat(Below) < parseFloat(Meet)) {
-                  if (!(parseFloat(Meet) < parseFloat(Exceed))) {
-                    callback('Target Must be Ascending/Descending');
-                  }
-                } else if (parseFloat(Below) > parseFloat(Meet)) {
-                  if (!(parseFloat(Meet) > parseFloat(Exceed))) {
-                    callback('Target Must be Ascending/Descending');
-                  }
-                } else {
-                  callback('Target Must be Ascending/Descending');
-                }
+            for (let index = 0; index < datas.length - 1; index++) {
+              if (parseFloat(dataMetrics[datas[index]]) > parseFloat(dataMetrics[datas[index + 1]])) {
+                isSortedDesc = true;
+              } else {
+                isSortedDesc = false;
+                break;
               }
             }
-            break;
-          case 'Exceed':
-            if (Exceed) {
-              if (regexZero.test(Exceed)) {
-                callback('Value must be not a zero');
-              } else if (!regexNumber.test(Exceed)) {
-                callback('Value must be a number');
-              } else if (Below && Meet && Exceed) {
-                if (parseFloat(Below) < parseFloat(Meet)) {
-                  if (!(parseFloat(Meet) < parseFloat(Exceed))) {
-                    callback('Target Must be Ascending/Descending');
-                  }
-                } else if (parseFloat(Below) > parseFloat(Meet)) {
-                  if (!(parseFloat(Meet) > parseFloat(Exceed))) {
-                    callback('Target Must be Ascending/Descending');
-                  }
-                } else {
-                  callback('Target Must be Ascending/Descending');
-                }
-              }
-            }
-            break;
-          default:
-                // code block
+          }
+          if (!isSortedAsc && !isSortedDesc) {
+            callback('Target Must be Ascending/Descending');
+          }
         }
       }
     }
@@ -108,53 +86,45 @@ export const metricValidatorText = (data) => [
   },
   {
     validator: async (rule, value, callback, source) => {
-      const Below = data.form.getFieldValue(`${data.type}[${data.indexarr}].Below`);
-      const Meet = data.form.getFieldValue(`${data.type}[${data.indexarr}].Meet`);
-      const Exceed = data.form.getFieldValue(`${data.type}[${data.indexarr}].Exceed`);
+      const { record } = data;
+      let dataMetrics = record.metrics.map((metric) => {
+        return `{"${metric.label}":
+        "${data.form.getFieldValue(
+          `${data.type}[${data.indexarr}].${metric.label}`
+          )}"}`;
+      });
+      dataMetrics = JSON.parse(`[${dataMetrics.toString()}]`);
+      dataMetrics = dataMetrics.reduce((result, current) => {
+        return Object.assign(result, current);
+      }, {});
+      let isFilled = true;
+      const datas = Object.keys(dataMetrics);
+      const datass = Object.values(dataMetrics);
       const regexNumber = new RegExp('^[0-9]*$');
-      // const regexZero = new RegExp('^[0-0]*$');
-      switch (data.title) {
-        case 'Below':
-          if (Below) {
-            if (regexNumber.test(Below)) {
-              callback('Value must contain number and letter, or letter only.');
-            } else if (Below && Meet && Exceed) {
-              if (Below === Meet) {
-                callback('Value must different with Meet');
-              } else if (Below === Meet) {
-                callback('Value must different with Exceed');
-              }
+      if (value) {
+        if (regexNumber.test(value)) {
+          callback('Value must contain number and letter, or letter only.');
+        } else {
+          for (let index = 0; index < datas.length; index++) {
+            if (!dataMetrics[datas[index]]) {
+              isFilled = false;
+              break;
             }
           }
-          break;
-        case 'Meet':
-          if (Meet) {
-            if (regexNumber.test(Meet)) {
-              callback('Value must contain number and letter, or letter only.');
-            } else if (Below && Meet && Exceed) {
-              if (Meet === Below) {
-                callback('Value must different with Below');
-              } else if (Meet === Exceed) {
-                callback('Value must different with Exceed');
+          if (isFilled) {
+            const data1 = datass.map(
+              (d) => (datass.filter((i) => i === d)).length
+            );
+            // eslint-disable-next-line array-callback-return
+            data1.filter((a, i) => {
+              if (a > 1) {
+                if (data.title === datas[i]) {
+                  callback('Value must be different');
+                }
               }
-            }
+            });
           }
-          break;
-        case 'Exceed':
-          if (Exceed) {
-            if (regexNumber.test(Exceed)) {
-              callback('Value must contain number and letter, or letter only.');
-            } else if (Below && Meet && Exceed) {
-              if (Exceed === Meet) {
-                callback('Value must different with Meet');
-              } else if (Exceed === Below) {
-                callback('Value must different with Below');
-              }
-            }
-          }
-          break;
-        default:
-              // code block
+        }
       }
     }
   }
