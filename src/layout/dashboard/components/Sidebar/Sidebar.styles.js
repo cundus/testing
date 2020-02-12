969689a34912ccbc6b73e252afbd146d37cc587a
aqil gifari <aqilgifari0@gmail.com>
2020-02-12 22:24:25 +0700

    layout fix

--- src/layout/dashboard/components/Sidebar/Sidebar.styles.js
+++ src/layout/dashboard/components/Sidebar/Sidebar.styles.js
@@ -1,6 +1,7 @@
 const styles = {
   sidebarContainer: {
-    zIndex: 1,
+    zIndex: 99,
+    position: 'fixed',
     boxShadow: '0px 1px 9px -3px rgba(0, 0, 0, 0.75)'
   },
   sidebarMenuWrapper: {
