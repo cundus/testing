c19ef072a571fc7b66ffbe60887432896bcb33be
aqil gifari <aqilgifari0@gmail.com>
2020-02-13 23:06:54 +0700

    notifications fix

--- src/layout/dashboard/components/Header/components/menus.js
+++ src/layout/dashboard/components/Header/components/menus.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Menu, List, Typography, Divider } from 'antd';
+import {
+ Menu, List, Typography, Divider, Empty, Icon
+} from 'antd';
 import { Link } from 'react-router-dom';
 import { navigatorManager, navigatorEmp } from '../../../../../utils/navigatorNotif';
 
@@ -26,33 +28,63 @@ export const langMenu = (
 export const notifMenu = (data, userId) => () => {
   const navigate = (item) => () => {
     if (item.userId !== userId) {
-      return navigatorManager(item.currentStep, userId);
+      return navigatorManager(item.currentStep, item.userId);
     } else {
       return navigatorEmp(item.currentStep);
     }
   };
 
+  const dateToday = (lastDate) => {
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneWeek = oneDay * 7;
+    const oneMonth = oneWeek * 4;
+    const ldate = new Date(lastDate);
+    const ndate = new Date();
+    const diffDays = Math.round(Math.abs((ndate - ldate) / oneDay));
+    const diffWeeks = Math.round(Math.abs((ndate - ldate) / oneWeek));
+    const diffMos = Math.round(Math.abs((ndate - ldate) / oneMonth));
+    if (diffDays === 1) {
+      return 'Today';
+    } else if (diffDays < 7) {
+      return `${diffDays} days ago`;
+    } else if (diffWeeks <= 1) {
+      return 'a week ago';
+    } else if (diffWeeks < 4) {
+      return `${diffWeeks} weeks ago`;
+    } else if (diffMos <= 1) {
+      return 'a month ago';
+    } else if (diffMos <= 4) {
+      return `${diffMos} months ago`;
+    } else {
+      return lastDate;
+    }
+  };
+
   return (
-    <List
-      style={{ width: 300, maxHeight: '100vh', padding: 0 }}
-      header={
-        <div>
-          <div style={{ textAlign: 'center', paddingTop: 20 }}>
-            <Typography.Text style={{ fontSize: 18 }}>User Notifications </Typography.Text>
-            {/* <Typography.Text mark>2 New!</Typography.Text> */}
-          </div>
-          <Divider />
-        </div>
-        }
-      dataSource={data}
-      // eslint-disable-next-line react/jsx-no-bind
-      renderItem={(item) => (
-        <Link to={navigate(item)}>
-          <List.Item>
-            <Typography.Paragraph>{item.name}</Typography.Paragraph>
-          </List.Item>
-        </Link>
-      )}
-    />
+    <Menu style={{ width: 300, maxHeight: '100vh' }}>
+      <div style={{
+        backgroundColor: '#F9FAFE',
+        paddingTop: 20,
+        paddingBottom: 20,
+        textAlign: 'center'
+      }}
+      >
+        <Typography.Text strong style={{ fontSize: 18 }}>User Notifications</Typography.Text>
+      </div>
+      <Divider style={{ margin: 0 }} />
+      {data.length !== 0 ? data.map((item, index) => (
+        <Menu.Item key={index} style={{ whiteSpace: 'break-spaces' }}>
+          <Link to={navigate(item)}>
+            <Typography.Paragraph style={{ marginBottom: 0 }}>{item.name}</Typography.Paragraph>
+            <Typography.Text style={{ marginTop: 0, fontSize: 10 }}>{dateToday(item.lastUpdate)}</Typography.Text>
+          </Link>
+        </Menu.Item>
+      )) :
+      <div>
+        <Empty
+          description="There's no notifications for you"
+        />
+      </div>}
+    </Menu>
   );
 };
