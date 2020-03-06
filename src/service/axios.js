545835772614163424992543ba7fdda9bcea9603
aqil gifari <aqilgifari0@gmail.com>
2020-03-06 15:43:48 +0700

    fix: handle add pathname

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,14 +1,25 @@
 import axios from 'axios';
+
 const uuidv1 = require('uuid/v1');
 
 const { REACT_APP_API_URL } = process.env;
 
 // custom config to create a new instance
 export const customAxios = axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? REACT_APP_API_URL : `${window.location.href.replace(window.location.pathname)}api`
+  baseURL:
+    process.env.NODE_ENV !== 'development' ?
+      REACT_APP_API_URL :
+      `${
+          window.location.pathname !== '/' ?
+          `${window.location.href.replace(window.location.pathname, '')}api` :
+          'api'
+        }`
 });
 
 customAxios.interceptors.request.use((config) => {
-  config.headers = Object.assign(config.headers , { 'X-Requested-With' : `${uuidv1()}-${new Date().getTime()}` });
+  // eslint-disable-next-line no-param-reassign
+  config.headers = Object.assign(config.headers, {
+    'X-Requested-With': `${uuidv1()}-${new Date().getTime()}`
+  });
   return config;
 });
