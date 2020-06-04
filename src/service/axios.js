6339d491d68c3debbc4d930036187ce519844bc8
Aqil Gifari <aqilgifari0@gmail.com>
2020-06-04 12:26:00 +0700

    die dynamic RCT API URL

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import apiUrl from '../utils/apiUrl';
+const { REACT_APP_API_URL } = process.env;
 
 const uuidv1 = require('uuid/v1');
 
 // custom config to create a new instance
 export const customAxios = axios.create({
-  baseURL: apiUrl()
+  baseURL: REACT_APP_API_URL
 });
 
 customAxios.interceptors.request.use((config) => {
