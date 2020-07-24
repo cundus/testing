26e9b9055dc2128ca96277010f6c1f08caea099a
aqigif <aqilgifari0@gmail.com>
2020-07-24 11:25:00 +0700

    fix redirect uri issue

--- src/service/auth/auth.js
+++ src/service/auth/auth.js
@@ -11,8 +11,8 @@ const config = {
   auth: {
     clientId: REACT_APP_CLIENTID_AADMS,
     authority: REACT_APP_AUTHORITY_AADMS,
-    redirectURI: redirectAadmsurl(),
-    postLogoutRedirectUri: redirectAadmsurl()
+    redirectUri: REACT_APP_REDIRECT_URI_AADMS,
+    postLogoutRedirectUri: REACT_APP_REDIRECT_URI_AADMS
   },
   cache: {
     cacheLocation: 'localStorage',
