b6ea99c2341878dc7da3201cc153cdf63f350336
Aqil Gifari <aqilgifari0@gmail.com>
2022-03-04 15:45:03 +0700

    debug undefined request

--- src/service/axios.js
+++ src/service/axios.js
@@ -3,7 +3,11 @@ import { INVALID_LOGIN_TOKEN, NOT_AUTHORIZED } from "../redux/status-code-type";
 import apiUrl from "../utils/apiUrl";
 import { authProvider } from "./activeDirectory";
 import { loginByADToken } from "./auth";
-const { REACT_APP_API_URL, REACT_APP_API_TIMEOUT, REACT_APP_MAX_REQUESTS_COUNT} = process.env;
+const {
+  REACT_APP_API_URL,
+  REACT_APP_API_TIMEOUT,
+  REACT_APP_MAX_REQUESTS_COUNT,
+} = process.env;
 
 const MAX_REQUESTS_COUNT = parseInt(REACT_APP_MAX_REQUESTS_COUNT);
 const INTERVAL_MS = 10;
@@ -19,8 +23,9 @@ export const customAxios = axios.create({
 });
 
 customAxios.interceptors.request.use(async (config) => {
+  console.log(config);
   return new Promise((resolve, reject) => {
-    let interval = setInterval( async() => {
+    let interval = setInterval(async () => {
       if (PENDING_REQUESTS < MAX_REQUESTS_COUNT) {
         PENDING_REQUESTS++;
         clearInterval(interval);
@@ -42,31 +47,37 @@ customAxios.interceptors.request.use(async (config) => {
   });
 });
 
-customAxios.interceptors.response.use(async function (response) {
-  PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1)
-  // console.log(response)
-  if (REFRESH_TOKEN <= 2 && (response?.data?.status_code === NOT_AUTHORIZED || response?.data?.status_code === INVALID_LOGIN_TOKEN)) {
-    const originalRequest = response.config;
-    const token = await authProvider.getAccessToken();
-    try {
-      const res = await loginByADToken(token?.accessToken)
-      if (res?.data?.status_code === 0) {
-        REFRESH_TOKEN++;
-        const result = res?.data?.result;
-        await localStorage.setItem('sfToken', result?.accessToken);
-        customAxios.defaults.headers.common['Authorization'] = result?.accessToken
-          ? `Bearer ${result?.accessToken}`
-          : "";
-        return customAxios(originalRequest);
+customAxios.interceptors.response.use(
+  async function (response) {
+    PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1);
+    // console.log(response)
+    if (
+      REFRESH_TOKEN <= 2 &&
+      (response?.data?.status_code === NOT_AUTHORIZED ||
+        response?.data?.status_code === INVALID_LOGIN_TOKEN)
+    ) {
+      const originalRequest = response.config;
+      const token = await authProvider.getAccessToken();
+      try {
+        const res = await loginByADToken(token?.accessToken);
+        if (res?.data?.status_code === 0) {
+          REFRESH_TOKEN++;
+          const result = res?.data?.result;
+          await localStorage.setItem("sfToken", result?.accessToken);
+          customAxios.defaults.headers.common["Authorization"] =
+            result?.accessToken ? `Bearer ${result?.accessToken}` : "";
+          return customAxios(originalRequest);
+        }
+      } catch (error) {
+        Promise.reject(error);
       }
-    } catch (error) {
-      Promise.reject(error)
+    } else {
+      REFRESH_TOKEN = 0;
+      return Promise.resolve(response);
     }
-  } else {
-    REFRESH_TOKEN = 0;
-    return Promise.resolve(response)
+  },
+  function (error) {
+    PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1);
+    return Promise.reject(error);
   }
-}, function (error) {
-  PENDING_REQUESTS = Math.max(0, PENDING_REQUESTS - 1)
-  return Promise.reject(error)
-})
+);
