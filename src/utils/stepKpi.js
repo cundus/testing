d9a95db9d0a982024cb45e77867f6c2752a9e847
Aqil Gifari <aqilgifari0@gmail.com>
2022-03-14 12:07:51 +0700

    prettier

--- src/utils/stepKpi.js
+++ src/utils/stepKpi.js
@@ -1,17 +1,19 @@
-export const EMP_GOAL_SETTING = 'Emp Goal Setting';
-export const MANAGER_GOAL_REVIEW = 'Manager Goal Review';
-export const PERFORMANCE_REVIEW_EMPLOYEE = 'Performance Review Employee';
-export const PERFORMANCE_REVIEW_MANAGER = 'Performance Review Manager';
-export const MANAGER_ACKNOWLEDMENT = 'Manager Acknowledgement And 1:1 Meeting';
-export const COMPILING_PROCESS = 'Compiling Process';
-export const EMP_ACKNOWLEDGEMENT = 'Emp Acknowledgement';
-export const COMPLETED = 'Completed';
-export const PROGRESS_MONITORING_1 = 'Progress Monitoring 1';
-export const PROGRESS_MONITORING_2 = 'Progress Monitoring 2';
-export const PROGRESS_REVIEW_ALIGNMENT_1 = 'Performance Review Alignment 1';
-export const PROGRESS_REVIEW_ALIGNMENT_2 = 'Performance Review Alignment 2';
-export const PROGRESS_REVIEW_ALIGNMENT_1_CALLIBRATION_1 = 'Performance Review Alignment 1 (Calibration 1)';
-export const PROGRESS_REVIEW_ALIGNMENT_2_CALLIBRATION_2 = 'Performance Review Alignment 2 (Calibration 2)';
+export const EMP_GOAL_SETTING = "Emp Goal Setting";
+export const MANAGER_GOAL_REVIEW = "Manager Goal Review";
+export const PERFORMANCE_REVIEW_EMPLOYEE = "Performance Review Employee";
+export const PERFORMANCE_REVIEW_MANAGER = "Performance Review Manager";
+export const MANAGER_ACKNOWLEDMENT = "Manager Acknowledgement And 1:1 Meeting";
+export const COMPILING_PROCESS = "Compiling Process";
+export const EMP_ACKNOWLEDGEMENT = "Emp Acknowledgement";
+export const COMPLETED = "Completed";
+export const PROGRESS_MONITORING_1 = "Progress Monitoring 1";
+export const PROGRESS_MONITORING_2 = "Progress Monitoring 2";
+export const PROGRESS_REVIEW_ALIGNMENT_1 = "Performance Review Alignment 1";
+export const PROGRESS_REVIEW_ALIGNMENT_2 = "Performance Review Alignment 2";
+export const PROGRESS_REVIEW_ALIGNMENT_1_CALLIBRATION_1 =
+  "Performance Review Alignment 1 (Calibration 1)";
+export const PROGRESS_REVIEW_ALIGNMENT_2_CALLIBRATION_2 =
+  "Performance Review Alignment 2 (Calibration 2)";
 
 export const stepKpisDropdown = [
   EMP_GOAL_SETTING,
@@ -25,7 +27,7 @@ export const stepKpisDropdown = [
   COMPILING_PROCESS,
   MANAGER_ACKNOWLEDMENT,
   EMP_ACKNOWLEDGEMENT,
-  COMPLETED
+  COMPLETED,
 ];
 
 const stepKpi = [
@@ -36,17 +38,14 @@ const stepKpi = [
   COMPILING_PROCESS,
   MANAGER_ACKNOWLEDMENT,
   EMP_ACKNOWLEDGEMENT,
-  COMPLETED
+  COMPLETED,
 ];
 
-export const stepKpiMonitoring = [
-  PROGRESS_MONITORING_1,
-  PROGRESS_MONITORING_2
-];
+export const stepKpiMonitoring = [PROGRESS_MONITORING_1, PROGRESS_MONITORING_2];
 
 export const stepKpiAlignment = [
   PROGRESS_REVIEW_ALIGNMENT_1,
-  PROGRESS_REVIEW_ALIGNMENT_2
+  PROGRESS_REVIEW_ALIGNMENT_2,
 ];
 
 export const menuMonitoringAllow = [
@@ -59,7 +58,7 @@ export const menuMonitoringAllow = [
   COMPILING_PROCESS,
   MANAGER_ACKNOWLEDMENT,
   EMP_ACKNOWLEDGEMENT,
-  COMPLETED
+  COMPLETED,
 ];
 
 export const menuAppraisalAllow = [
@@ -70,7 +69,7 @@ export const menuAppraisalAllow = [
   COMPILING_PROCESS,
   MANAGER_ACKNOWLEDMENT,
   EMP_ACKNOWLEDGEMENT,
-  COMPLETED
+  COMPLETED,
 ];
 
 export default stepKpi;
