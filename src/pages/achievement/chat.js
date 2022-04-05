5a35e4afef05b5879ece53d0084432eb34511d5f
sundusardian <sundusardian@gmail.com>
2022-04-05 11:42:09 +0700

    remove unused variables

--- src/pages/achievement/chat.js
+++ src/pages/achievement/chat.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Button,
   Modal,
@@ -9,21 +9,25 @@ import {
   Spin,
   Form,
   Avatar,
-  Card
-} from 'antd';
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router';
-import { connect } from 'react-redux';
-import { getListActivity, getActivityStatus, getActivityThreadChat, createChat } from '../../redux/actions/activity';
-import TableActivity from './tableActivity';
-import FormSend from './component/form';
-import globalStyle from '../../styles/globalStyles';
-import apiUrl from '../../utils/apiUrl';
+  Card,
+} from "antd";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router";
+import { connect } from "react-redux";
+import {
+  getListActivity,
+  getActivityStatus,
+  getActivityThreadChat,
+  createChat,
+} from "../../redux/actions/activity";
+import TableActivity from "./tableActivity";
+import FormSend from "./component/form";
+import globalStyle from "../../styles/globalStyles";
+import apiUrl from "../../utils/apiUrl";
 
 const { confirm } = Modal;
 const { Text, Title } = Typography;
 const { TextArea } = Input;
-
 
 class Chat extends Component {
   constructor(props) {
@@ -31,9 +35,9 @@ class Chat extends Component {
     this.state = {
       dataSource: [],
       visible: false,
-      titleForm: '',
+      titleForm: "",
       activityStatus: [],
-      load: true
+      load: true,
     };
   }
 
@@ -42,24 +46,19 @@ class Chat extends Component {
   }
 
   getAllData = async () => {
-    const {
-      GetThreadActivity,
-      GetActivityStatus,
-      GetActivityThreadChat,
-      match
-    } = this.props;
+    const { GetThreadActivity, GetActivityThreadChat, match } = this.props;
     const { params } = match;
     const { idActivity, idThread } = params;
     await GetThreadActivity(idActivity);
     await GetActivityThreadChat(idActivity, idThread);
-    if ( this.props.chat.feedbacks !== undefined) {
+    if (this.props.chat.feedbacks !== undefined) {
       this.setState({ load: false });
     }
   };
 
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
-    const { form, match, doFeedback, chat} = this.props;
+    const { form, match, doFeedback } = this.props;
     const { params } = match;
     const { idThread } = params;
     const { validateFields } = form;
@@ -91,63 +90,92 @@ class Chat extends Component {
             This is an Online activity feedback session with your supperior.
           </Text>
           <Divider />
-          {
-            loadingActivityChat || loadingActivity || load ?
-            <center><Spin/></center> :
+          {loadingActivityChat || loadingActivity || load ? (
+            <center>
+              <Spin />
+            </center>
+          ) : (
             <div>
               <center>
-                <Title level={4}>{`KPI : ${ kpiName|| ''}`}</Title>
+                <Title level={4}>{`KPI : ${kpiName || ""}`}</Title>
                 <br />
               </center>
               <Title level={4}>{chatName}</Title>
               <Avatar
                 shape="circle"
-                size={'large'}
+                size={"large"}
                 src={`${apiUrl()}/user/photo/${userId}`}
                 icon="user"
                 className="avatar"
                 type="rounded"
-              />&nbsp;
+              />
+              &nbsp;
               <Text>Created By {fullName}</Text>
-              <Divider/>
-              <br/>
-              <ul style={{textDecoration: 'none', listStyle: 'none'}}>
-                { feedbacks ?
-                  feedbacks.map(d => (
+              <Divider />
+              <br />
+              <ul style={{ textDecoration: "none", listStyle: "none" }}>
+                {feedbacks ? (
+                  feedbacks.map((d) => (
                     <li>
-                      <Card style={{border: 'none'}}>
-                      <Avatar
-                      shape="circle"
-                      size={'default'}
-                      src={`${apiUrl()}/user/photo/${d.userId}`}
-                      icon="user"
-                      className="avatar"
-                    />&nbsp;
-                      <Text strong level={1} style={{display: 'inline', fontSize: '15px'}}>{d.feedback}</Text>
-                      <br/>
-                      <Text strong level={1} style={{display: 'inline', fontSize: '10px'}}>{d.fullName}</Text> - <Text strong level={1} style={{display: 'inline', fontSize: '10px'}}>{d.creationDate}</Text>
+                      <Card style={{ border: "none" }}>
+                        <Avatar
+                          shape="circle"
+                          size={"default"}
+                          src={`${apiUrl()}/user/photo/${d.userId}`}
+                          icon="user"
+                          className="avatar"
+                        />
+                        &nbsp;
+                        <Text
+                          strong
+                          level={1}
+                          style={{ display: "inline", fontSize: "15px" }}
+                        >
+                          {d.feedback}
+                        </Text>
+                        <br />
+                        <Text
+                          strong
+                          level={1}
+                          style={{ display: "inline", fontSize: "10px" }}
+                        >
+                          {d.fullName}
+                        </Text>{" "}
+                        -{" "}
+                        <Text
+                          strong
+                          level={1}
+                          style={{ display: "inline", fontSize: "10px" }}
+                        >
+                          {d.creationDate}
+                        </Text>
                       </Card>
                     </li>
-
-                  )): <div></div>
-                }
+                  ))
+                ) : (
+                  <div></div>
+                )}
               </ul>
             </div>
-          }
+          )}
         </div>
         <Form onSubmit={this.handleSubmit}>
           <Form.Item>
-            {getFieldDecorator('comment', {
-              rules: [{ required: true, message: 'please insert your chat' }],
-            })(
-              <TextArea placeholder={'What do you want to say ?'}/>
-            )}
+            {getFieldDecorator("comment", {
+              rules: [{ required: true, message: "please insert your chat" }],
+            })(<TextArea placeholder={"What do you want to say ?"} />)}
           </Form.Item>
-          <Button type="primary" htmlType="submit" className="login-form-button">Send</Button>
-          </Form>
-          <center>
-          <Button onClick={()=> this.props.history.goBack()} >Back</Button>
-          </center>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Send
+          </Button>
+        </Form>
+        <center>
+          <Button onClick={() => this.props.history.goBack()}>Back</Button>
+        </center>
       </div>
     );
   }
@@ -156,20 +184,18 @@ class Chat extends Component {
 const mapStateToProps = (state) => ({
   activityThread: state.ActivityReducer,
   activityStatus: state.ActivityStatusReducer,
-  chat: state.ActivityChatReducer
+  chat: state.ActivityChatReducer,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  GetThreadActivity: (activityID)=> dispatch(getListActivity(activityID)),
-  GetActivityStatus: ()=> dispatch(getActivityStatus()),
-  GetActivityThreadChat: (idActivity, idChat) => dispatch(getActivityThreadChat(idActivity, idChat)),
-  doFeedback: (data) => dispatch(createChat(data))
+  GetThreadActivity: (activityID) => dispatch(getListActivity(activityID)),
+  GetActivityStatus: () => dispatch(getActivityStatus()),
+  GetActivityThreadChat: (idActivity, idChat) =>
+    dispatch(getActivityThreadChat(idActivity, idChat)),
+  doFeedback: (data) => dispatch(createChat(data)),
 });
 
-const connectToComponent = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Chat);
+const connectToComponent = connect(mapStateToProps, mapDispatchToProps)(Chat);
 
 export default Form.create({})(withRouter(connectToComponent));
 
@@ -179,5 +205,5 @@ Chat.propTypes = {
   getKpiList: PropTypes.func,
   submitNext: PropTypes.func,
   stepChange: PropTypes.func,
-  form: PropTypes.instanceOf(Object)
+  form: PropTypes.instanceOf(Object),
 };
